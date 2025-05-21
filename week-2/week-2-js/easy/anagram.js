9324d0e4f802113df40343d696d44b3ef5698e3e
TitanX311 <abirdebnath311@hotmail.com>
2025-05-21 21:45:18 +0530

    easy 1st solved

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -6,6 +6,27 @@
 
 function isAnagram(str1, str2) {
 
+	let sortedStr1 = []
+	let sortedStr2 = []
+
+	str1 = str1.toLowerCase();
+	str2 = str2.toLowerCase();
+	
+	for ( let i of str1) {
+		sortedStr1.push(i)
+	}
+	for ( let i of str2) {
+		sortedStr2.push(i)
+	}
+
+	sortedStr1 = sortedStr1.sort().join("");
+	sortedStr2 = sortedStr2.sort().join("");
+
+	if (sortedStr1 === sortedStr2) {
+		return true
+	} else {
+		return false
+	}
 }
 
 module.exports = isAnagram;
